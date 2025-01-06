c0862ce1e37f82f93659c7cac72a2c8a126a7414
AKA Math <amithjkamath@users.noreply.github.com>
2025-01-06 16:54:29 +0100

    function to create datastore

--- createmedmnistdatastore.m
+++ createmedmnistdatastore.m
@@ -8,5 +8,23 @@ function imds = createmedmnistdatastore(images, labels)
     %    Outputs:
     %        imds: image datastore ready to use for training/evaluation.
     
-    imds = [];
+    n_images = size(images, 1);
+    assert(length(labels) == n_images, ...
+        "Number of images must match number of labels")
+
+    save_dir = uigetdir(".", "Choose folder to save image data in:");
+
+    unique_labels = unique(labels);
+    for label_idx = 1:numel(unique_labels)
+        mkdir(fullfile(save_dir, string(unique_labels(label_idx))));
+    end
+
+    for i = 1:n_images
+        image_data = squeeze(images(i, :, :, :));
+        label_data = labels(i);
+        i_padded = sprintf('%04d', i);
+        imwrite(image_data, fullfile(save_dir, string(label_data), ...
+                string(i_padded) + ".png"));
+    end
+    imds = imageDatastore(save_dir, "IncludeSubfolders", true, "LabelSource","foldernames");
 end
